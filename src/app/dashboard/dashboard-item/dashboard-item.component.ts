4e8740f33da1cddf06e15161570cfe7346f8ba8b
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-03-05 09:26:53 -0500

    fixed the dashboard component

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -11,6 +11,6 @@ export class DashboardItemComponent {
   // @Input({ required: true }) image!: { path: string; alt: string };
   // @Input({ required: true }) title!: string;
 
-  image = input.required<{ path: string; alt: string }>();
+  image = input.required<{ src: string; alt: string }>();
   title = input.required<string>();
 }
